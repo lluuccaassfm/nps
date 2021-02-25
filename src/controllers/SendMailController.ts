f7fcdae2af45e1c78514c8fab51061369b1670dd
Lucas Lima <lucas.lima@foton.la>
2021-02-25 18:23:34 -0300

    Adiciona mapeamento dos relacionamento

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -42,7 +42,8 @@ class SendMailController {
 
     // Verifica se já enviou algum email para o usuário 
     const surveyUserAlreadExist = await surveysUsersRepository.findOne({
-      where: [{user_id: user.id}, {value: null}]
+      where: [{user_id: user.id}, {value: null}],
+      relations: ["user", "survey"]
     });
     if(surveyUserAlreadExist){
       await SendMailService.execute(email, survey.title, variables, npsPath);
